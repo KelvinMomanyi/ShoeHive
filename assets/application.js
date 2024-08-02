4118e2dfb46a6e8f8ea7e9147514c58da68a0c0e
KelvinMomanyi <kelvinmata42@gmail.com>
2024-08-02 12:11:55 +0300

    new

--- assets/application.js
+++ assets/application.js
@@ -105,13 +105,16 @@ addEventOnElem(navbarLinks, "click", closeNavbar)
 
 const header = document.querySelector("[data-header]")
 const backTopBtn = document.querySelector("[data-back-top-btn]")
+const headerAction = document.querySelector("[data-headerActions]")
 const headerActive = function () {
   if (window.scrollY > 150) {
     header.classList.add("active");
     backTopBtn.classList.add("active");
+    headerAction.classList.add("active");
   } else {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
+    headerAction.classList.remove("active");
   }
 }
 
